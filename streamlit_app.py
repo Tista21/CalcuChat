becb6530ca4a72cf9f7dd7da8ab7e96a3b2e0af2
Tista21 <109456894+Tista21@users.noreply.github.com>
2025-05-09 17:49:56 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ def generate_response(input_text):
   st.info(llm(input_text))
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What is the capital of delhi')
+  text = st.text_area('Enter text:', 'What is the capital of india')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
