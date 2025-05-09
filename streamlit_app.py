1017a0cc3df8b96a5a51e22c87e1cb69e1bf6f92
Tista21 <109456894+Tista21@users.noreply.github.com>
2025-05-09 18:19:00 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ openai_api_key = st.sidebar.text_input('OpenAI API Key')
 
 def generate_response(input_text):
   @tool
-  def multiply(a: int, b: int) -> int:
+  def multiply(a: int, b: int) -> str:
     """Given 2 numbers a and b this tool returns their product"""
     return a * b
   llm = ChatOpenAI(openai_api_key=openai_api_key)
@@ -21,13 +21,13 @@ def generate_response(input_text):
   tool_result = multiply.invoke(result.tool_calls[0])
   messages.append(tool_result)
   messages
-  text = llm_with_tools.invoke(messages)
+  text = llm_with_tools.invoke(messages).content
   st.info(llm(input_text))
 
 with st.form('my_form'):
-  text = st.text_area('Enter text:', 'What is the capital of india')
+  text = st.text_area('Enter text:', 'What multiplication of 2 and 4')
   submitted = st.form_submit_button('Submit')
   if not openai_api_key.startswith('sk-'):
     st.warning('Please enter your OpenAI API key!', icon='⚠')
   if submitted and openai_api_key.startswith('sk-'):
-    generate_response(text.content)
+    generate_response(text)
